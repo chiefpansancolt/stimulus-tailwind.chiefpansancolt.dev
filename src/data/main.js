4c76791076771d4226f94c1cf89115f00aa54d43
Christopher Pezza <chris.pezza@hey.com>
2023-04-02 09:47:50 -0400

    add data files

---
+++ src/data/main.js
@@ -0,0 +1,27 @@
+import { BookOpenIcon, ChatBubbleBottomCenterTextIcon, MapIcon, PlayCircleIcon } from '@heroicons/react/24/outline';
+
+
+const main = [
+  {
+    title: "Documentation",
+    href: "/",
+    icon: BookOpenIcon,
+  },
+  {
+    title: "Examples",
+    href: "https://chiefpansancolt.github.io/stimulus-tailwind-playground/",
+    icon: PlayCircleIcon,
+  },
+  {
+    title: "Community",
+    href: "https://github.com/chiefpansancolt/stimulus-tailwind-components/discussions",
+    icon: ChatBubbleBottomCenterTextIcon,
+  },
+  {
+    title: "Roadmap",
+    href: "https://github.com/users/chiefpansancolt/projects/7",
+    icon: MapIcon,
+  },
+]
+
+export default main
---
+++ src/data/navigation.js
@@ -0,0 +1,37 @@
+const navigation = [
+  {
+    title: "Introduction",
+    links: [
+      { title: "Getting started", href: "/" },
+      { title: "Installation", href: "/docs/installation" },
+    ],
+  },
+  {
+    title: "Features",
+    links: [
+      { title: "Notification", href: "/docs/features/notification" },
+      { title: "Theme", href: "/docs/features/theme" },
+      { title: "Switch", href: "/docs/features/switch" },
+      { title: "Modal", href: "/docs/features/modal" }
+    ]
+  },
+  {
+    title: "Contributing",
+    links: [
+      { title: "How to contribute", href: "/docs/how-to-contribute" },
+      { title: "Sponsor", href: "/docs/sponsor" },
+      { title: "Change log", href: "/docs/change-log" },
+    ],
+  },
+  {
+    title: "Resources",
+    links: [
+      { title: "Tailwind Setup", href: "https://tailwindcss.com/docs/installation" },
+      { title: "StimulusJS Setup", href: "https://stimulus.hotwired.dev/handbook/installing" },
+      { title: "Tailwind Components", href: "https://tailwindui.com/" },
+      { title: "View Component Setup", href: "https://viewcomponent.org/" },
+    ],
+  },
+]
+
+export default navigation
